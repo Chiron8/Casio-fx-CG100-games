4d09754b9c91bcb6fdf557340a8b0d072647a4d2
Chiron <135429057+Chiron8@users.noreply.github.com>
2025-05-07 09:24:12 +0100

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -1,5 +1,9 @@
 from turtle import *
 
+pos = [[0, 0], [-100, -60], [-20, -60], [75, -60], [-100, -10], [-20, -10], [75, -10], [-100, 40], [-20, 40], [75, 40]]
+turn = 0
+won = False
+key= 0
 speed(10000)
 for i in [-50, 50]:
   penup()
@@ -14,3 +18,9 @@ for j in [-25, 25]:
   goto(125, j)
 
 penup()
+while won == False:
+	key = input("Enter Key: ")
+    setposition(pos[key])
+    pendown()
+    goto(xcor()+10, ycor()+10)
+    
