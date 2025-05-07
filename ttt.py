69b27a97ed97ceb1c5c9c56cc191e21e442a70ec
Chiron <135429057+Chiron8@users.noreply.github.com>
2025-05-07 08:27:50 +0100

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -1,13 +1,13 @@
 from turtle import *
 
 speed(10000)
-for i in range(-75, 75, 75):
+for i in [-50, 50]:
   penup()
   setposition(i, 75)
   pendown()
   goto(i, -75)
 
-for j in range(-10, 65, 30):
+for j in [-25, 25]:
   penup()
   setposition(-125, j)
   pendown()
