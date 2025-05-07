095bcab58a164cf595f6962e31137e154512293d
Chiron <135429057+Chiron8@users.noreply.github.com>
2025-05-07 12:27:41 +0100

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -1,15 +1,17 @@
 from turtle import *
-
+#variables
 pos = [[0, 0], [-100, -60], [-20, -60], [75, -60], [-100, -10], [-20, -10], [75, -10], [-100, 40], [-20, 40], [75, 40]]
 plays = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-
 turn = 0
-won = False
+won = 2
 key= 0
-speed(10000)
 
+#turtle config
+speed("fastest")
+color("blue")
 hideturtle()
 
+#grid
 for i in [-50, 50]:
   penup()
   setposition(i, 75)
@@ -22,12 +24,17 @@ for j in [-25, 25]:
   pendown()
   goto(125, j)
 
+#pause
 penup()
 for i in range(1, 10):
     goto(-100, -100)
     goto(100, 100)
 
-while won == False:
+#main loop
+while won == 2:
+  for i in range(1, 10):
+    goto(-100, -100)
+    goto(100, 100)
   key = int(input("Enter Key: "))
   if plays[key] == 2:
     setposition(pos[key][0], pos[key][1])
@@ -49,11 +56,27 @@ while won == False:
       turn = 1
       plays[key] = 0
       penup()
-    for i in range(1, 10):
-        goto(-100, -100)
-        goto(100, 100)
+
+    for i in range(1, 9):
+      if i == 1 or i == 4 or i == 9:
+        if plays[i] == plays[i+1] == plays[i+2]:
+          won = plays[i]
+      if i == 7 or i == 8 or i == 9:
+        if plays[i] == plays[i-3] == plays[i-6]:
+          won = plays[i]
+      if i == 1:
+        if plays[i] == plays[i+4] == plays[i+8]:
+          won = plays[1]
+      if i == 7:
+        if plays[1] == plays[i-2] == plays[i-4]:
+          won = plays[7]
   else:
     print("Already played, try again")
+
+if won = 0:
+  print("Noughts has won!")
+else:
+  print("Crosses has won!")
   
     
     
